190edc188d9c40c1f02525bd0c784b3e23e481fe
cookienator <rafivar@gmail.com>
2020-10-04 02:00:10 +0300

    Fixed bug (if path doesn't exist, app crashes upon save)

--- src/game/GameDataManager.kt
+++ src/game/GameDataManager.kt
@@ -2,23 +2,34 @@ package game
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import data.SerializableItemData
 import data.MazeSerializer
+import data.SerializableItemData
 import data.SerializableRoomData
 import map.MapBuilder
 import java.io.File
 
+
 class GameDataManager(private val player: Player, private val mapBuilder: MapBuilder) {
-    private val fileName = "save/file.txt"
-    private val file = File(fileName)
     private var builder: GsonBuilder = GsonBuilder().setPrettyPrinting()
     private var gson: Gson
+    private val file = getFile()
 
     init {
         builder.registerTypeAdapter(SerializableItemData::class.java, MazeSerializer<SerializableItemData>())
         builder.registerTypeAdapter(SerializableRoomData::class.java, MazeSerializer<SerializableRoomData>())
         gson = builder.create()
+    }
 
+    private fun getFile() : File {
+        val filePath = "save"
+        val fileName = "file.txt"
+        val directory = File(filePath)
+        if (!directory.exists()) {
+            directory.mkdir()
+            // If you require it to make the entire directory path including parents,
+            // use directory.mkdirs(); here instead.
+        }
+        return File("$filePath//$fileName")
     }
 
     fun save(): GameResult {
@@ -29,7 +40,6 @@ class GameDataManager(private val player: Player, private val mapBuilder: MapBui
     }
 
     fun load(): GameResult {
-        val file = File(fileName)
         val data = file.readText()
         val gameData = gson.fromJson(data, GameData::class.java)
         mapBuilder.loadData(gameData)
