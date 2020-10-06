0bae4da27fe6318a67d67677ba4357c5f7e25a19
cookienator <rafivar@gmail.com>
2020-10-06 04:59:59 +0300

    removed redundant classes ("DogRoom") + renamed rooms in MapBuilder

--- src/map/MapBuilder.kt
+++ src/map/MapBuilder.kt
@@ -11,7 +11,7 @@ import items.ItemMap
 import items.SavableItem
 import items.Tiger
 
-class MapBuilder() {
+class MapBuilder {
     private val rooms = MazeMap()
     private val gameItems = ItemMap()
     lateinit var player: Player
@@ -21,8 +21,8 @@ class MapBuilder() {
         val room1 = FirstRoom()
         val room2 = Room("room2")
         val room3 = Room("room3")
-        val roomWithTiger = DogRoom("dogRoom2")
-        val room6 = DogRoom("dogRoom1")
+        val roomWithTiger = Room("roomWithTiger")
+        val room4 = Room("room4")
         val room5 = Room("room5")
         val poison = Poison()
         val roomWithBowl = RoomWithBowl(poison)
@@ -39,33 +39,33 @@ class MapBuilder() {
         room3.addRoom(Direction.NORTH, roomWithTiger)
         roomWithTiger.addRoom(Direction.SOUTH, room3)
 
-        roomWithTiger.addRoom(Direction.NORTH, room6)
-        room6.addRoom(Direction.SOUTH, roomWithTiger)
+        roomWithTiger.addRoom(Direction.NORTH, room5)
+        room5.addRoom(Direction.SOUTH, roomWithTiger)
 
-        roomWithTiger.addRoom(Direction.EAST, room5)
-        room5.addRoom(Direction.WEST, roomWithTiger)
+        roomWithTiger.addRoom(Direction.EAST, room4)
+        room4.addRoom(Direction.WEST, roomWithTiger)
 
-        room5.addRoom(Direction.NORTH, roomWithBowl)
-        roomWithBowl.addRoom(Direction.SOUTH, room5)
+        room4.addRoom(Direction.NORTH, roomWithBowl)
+        roomWithBowl.addRoom(Direction.SOUTH, room4)
 
-        room6.addRoom(Direction.EAST, roomWithBowl)
-        roomWithBowl.addRoom(Direction.WEST, room6)
+        room5.addRoom(Direction.EAST, roomWithBowl)
+        roomWithBowl.addRoom(Direction.WEST, room5)
 
-        room6.addRoom(Direction.WEST, roomWithGuard)
-        roomWithGuard.addRoom(Direction.EAST, room6)
+        room5.addRoom(Direction.WEST, roomWithGuard)
+        roomWithGuard.addRoom(Direction.EAST, room5)
 
-        room6.addRoom(Direction.NORTH, exit)
+        room5.addRoom(Direction.NORTH, exit)
 
         //Set dog
         val tiger = Tiger()
         tiger.setStoppingItem(poison)
-        room6.addConstraint(Direction.NORTH, Constraint(tiger::isAlive, "Try distracting the [$tiger] first!"))
+        room5.addConstraint(Direction.NORTH, Constraint(tiger::isAlive, "Try distracting the [$tiger] first!"))
         roomWithTiger.addItem(tiger)
 
         //Save room data
         saveRoom(room1)
         saveRoom(roomWithBowl)
-        saveRoom(room6)
+        saveRoom(room5)
         saveRoom(roomWithTiger)
         return room1
     }
