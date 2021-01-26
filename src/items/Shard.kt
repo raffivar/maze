76fc9f0204491b9e4e834f7187ec9b173d73e252
cookienator <rafivar@gmail.com>
2021-01-26 16:14:10 +0200

    Modified shard description + removed redundant code

--- src/items/Shard.kt
+++ src/items/Shard.kt
@@ -5,18 +5,10 @@ import game.GameResultCode
 import items.interfaces.Reflective
 import player.Player
 
-class Shard : Item("Shard", "This is shiny."), Reflective {
-    enum class Error { NOT_IN_INVENTORY }
-
+class Shard : Item("Shard", "A piece of mirror. Might be useful."), Reflective {
     override fun take(player: Player): GameResult {
         player.inventory.addItem(this)
         player.currentRoom.removeItem(this)
         return GameResult(GameResultCode.SUCCESS, "Obtained [${this.name}].")
-    }
-
-    fun getErrorMessage(error: Error): String {
-        when (error) {
-            Error.NOT_IN_INVENTORY -> return "You can't peek anywhere without something shiny...!!"
-        }
     }
 }
